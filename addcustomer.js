26d082f9b227fd7d1a746e3b773946ce015343c8
akashkt-24 <129619464+akashkt-24@users.noreply.github.com>
2024-11-08 19:56:43 +0530

    Add files via upload

---
+++ addcustomer.js
@@ -0,0 +1,191 @@
+/*const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+const errorMessage = document.getElementById("error-message");
+let current = 1;
+
+nextBtnFirst.addEventListener("click", function(event){
+  // Check form validity
+  if (form.checkValidity()) {
+
+  //Hide error message if form is valid
+  errorMessage.style.display = "none";
+
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+} else {
+    // If form is invalid, let HTML5 validation messages show
+    form.reportValidity(); // This will show default HTML5 messages as well
+    errorMessage.innerText = "Please fill out all required fields";
+    errorMessage.style.display = "block";
+    event.preventDefault();
+  }
+  // Change slide and update progress only if form is valid
+  
+});
+nextBtnSec.addEventListener("click", function(event){
+
+  // Check form validity
+  if (form.checkValidity()) {
+
+  //Hide error message if form is valid
+  errorMessage.style.display = "none";
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+
+} else {
+    // If form is invalid, let HTML5 validation messages show
+    form.reportValidity(); // This will show default HTML5 messages as well
+    errorMessage.innerText = "Please fill out all required fields";
+    errorMessage.style.display = "block";
+    event.preventDefault();
+  }
+  // Change slide and update progress only if form is valid
+});
+nextBtnThird.addEventListener("click", function(event){
+  const form = document.querySelector("#form");
+
+  // Check form validity
+  if (form.checkValidity()) {
+
+  //Hide error message if form is valid
+  errorMessage.style.display = "none";
+
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+} else {
+    // If form is invalid, let HTML5 validation messages show
+    form.reportValidity(); // This will show default HTML5 messages as well
+    errorMessage.innerText = "Please fill out all required fields";
+    errorMessage.style.display = "block";
+    event.preventDefault();
+  }
+  // Change slide and update progress only if form is valid
+});
+submitBtn.addEventListener("click", function(){
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+  setTimeout(function(){
+    alert("Your Form Successfully Signed up");
+    location.reload();
+  },800);
+});
+
+
+  prevBtnSec.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "0%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+  });
+  prevBtnThird.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+  });
+  prevBtnFourth.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "-50%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+  });
+  */
+
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
+
+nextBtnFirst.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+submitBtn.addEventListener("click", function(){
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+  setTimeout(function(){
+    alert("Your Form Successfully Signed up");
+    location.reload();
+  },800);
+});
+
+prevBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFourth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
